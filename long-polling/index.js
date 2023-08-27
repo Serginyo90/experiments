9367eaf2bad950e5234fcd31920ac755c7a56706
Sergii Cherbadzhy <s.cherbadzhy@gmail.com>
2023-08-27 17:41:33 +0300

    Learn Long Polling

---
+++ long-polling/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+let data = null;
+
+app.get('/poll', (req, res) => {
+  const waitForData = () => {
+    console.log('waitForData_', data)
+    if (data) {
+      res.json({ data });
+      data = null;
+    } else {
+      setTimeout(waitForData, 3000);
+    }
+  };
+  
+  waitForData();
+});
+
+// Simulate an endpoint that receives data
+app.post('/send-data', (req, res) => {
+
+  console.log('_req_body_', req.body)
+  data = req.body;
+  res.send('Data received');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ long-polling/ui.js
@@ -0,0 +1,20 @@
+async function longPoll() {
+    try {
+      const response = await fetch('http://localhost:3000/poll');
+      const result = await response.json();
+  
+      console.log('Received data:', result.data);
+      
+      // Start the next poll
+      longPoll();
+    } catch (error) {
+      console.log('Polling error:', error);
+      
+      // Retry the poll if it fails
+      setTimeout(longPoll, 2000);
+    }
+  }
+  
+  // Kick off the long-polling loop
+  longPoll();
+  
