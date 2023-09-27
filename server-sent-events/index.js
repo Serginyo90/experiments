27938dd3d1fb1c6cf1450623054fe2d1da7dda2e
Sergii Cherbadzhy <s.cherbadzhy@gmail.com>
2023-09-27 13:20:46 +0300

    Learn CORS
    https://youtu.be/y63hfdRxho4

--- server-sent-events/index.js
+++ server-sent-events/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// It is from video about CORS https://youtu.be/y63hfdRxho4
+// app.use((req, res, next) => {
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // next();
+// });
+
 app.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
